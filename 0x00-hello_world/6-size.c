25e85fd44dd52e207dfe5232469197f684af07a5
Andres802 <reachandres@gmail.com>
2020-06-10 23:04:36 +0000

    C is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - size of evaluates the size of a variable
@@ -15,8 +15,8 @@ long long int longlongintType;
 
 printf("Size of char: %ld byte(s)\n", sizeof(charType));
 printf("Size of int: %ld byte(s)\n", sizeof(intType));
-printf("Size of longint: %ld byte(s)\n", sizeof(longintType));
-printf("Size of longlongint: %ld byte(s)\n", sizeof(longlongintType));
+printf("Size of long int: %ld byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %ld byte(s)\n", sizeof(longlongintType));
 printf("Size of float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
