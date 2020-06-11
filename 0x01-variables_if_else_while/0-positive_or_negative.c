14589135724b53b1d78c481e831413d3c1b50d92
Andres802 <reachandres@gmail.com>
2020-06-11 21:49:01 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,21 @@
- /**
- * main - Entry point
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - clasify an integer
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	/* if the number is greater than 0: is positive*/
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	/* if the number is 0: is zero*/
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	/* if the number is less than 0: is negative*/
-	else if (n < 0)
-	{
+	if (n < 0)
 		printf("%d is negative\n", n);
-	}
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	return (0);
 }
