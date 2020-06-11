2893b059a8af638ac7ab3eb358b174702e56a25b
Andres802 <reachandres@gmail.com>
2020-06-11 23:50:22 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - last digit
+ *
+ * Return: Always 0 (Success)
+ */
+# include <stdio.h>
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
