f2a0dfde11a24f34bcae9dd948bf3d2ae1414b70
Andres802 <reachandres@gmail.com>
2020-06-12 05:05:43 +0000

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,39 +6,39 @@
  */
 int main(void)
 {
-	int first;
-	int second;
-	int third;
-	int four;
+int first;
+int second;
+int third;
+int four;
 
-	for (first = 48; first < 58; first++)
-	{
-		for (second = 48; second < 58; second++)
-		{
-			for (third = first; third < 58; third++)
-			{
-				for (four = 48; four < 58; four++)
-				{
-					if (((first * 10) + second) != ((third * 10) + four))
-					{
-						if (((first * 10) + second) < ((third * 10) + four))
-						{
-							putchar(first);
-							putchar(second);
-							putchar(' ');
-							putchar(third);
-							putchar(four);
-							if (first < 57 || second < 56 || third < 57 || four < 57)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (first = 48; first < 58; first++)
+{
+for (second = 48; second < 58; second++)
+{
+for (third = first; third < 58; third++)
+{
+for (four = 48; four < 58; four++)
+{
+if (((first * 10) + second) != ((third * 10) + four))
+{
+if (((first * 10) + second) < ((third * 10) + four))
+{
+putchar(first);
+putchar(second);
+putchar(' ');
+putchar(third);
+putchar(four);
+if (first < 57 || second < 56 || third < 57 || four < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
