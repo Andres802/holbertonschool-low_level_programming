b88d248551d439bea939b8aa1da31e36bb6e8d0b
Andres802 <reachandres@gmail.com>
2020-06-12 03:06:54 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+int ch = 'a'
+
+	for (i = 48; i <= 57; i++)
+	putchar(i);
+	for (ch = 'a'; ch <= 'f'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
