cc1e90e34acc382376586ac67ded4d2cc7a6a387
Andres802 <reachandres@gmail.com>
2020-06-12 04:33:38 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 48; first < 57; first++)
+	{
+		for (second = first + 1; second < 57; second++)
+		{
+			putchar(first);
+			putchar(second);
+			if (first < 56 || second < 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
