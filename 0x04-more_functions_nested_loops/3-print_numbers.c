510d7eabd0e1a20157a3d7977e6ee6c5d8099bde
Andres802 <reachandres@gmail.com>
2020-06-19 02:23:35 +0000

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
- * print_numbers - print the numbers
- * void: none args
- *
+ * print_numbers - Prints the numbers from 0 to 9
+ * _putchar - Print characters one by one
+ * @void: Description of a empty parameter
+ * Description: This prints the numbers from 0 to 9
+ * Return: 0 Value if it works
  */
 void print_numbers(void)
 {
