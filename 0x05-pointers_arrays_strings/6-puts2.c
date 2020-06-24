d1f022093798ff6a72a3baf87b67945edcc245c2
Andres802 <reachandres@gmail.com>
2020-06-24 01:34:10 +0000

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include"holberton.h"
+/**
+ * puts2 - puts a value
+ * @str: and string parameter as a pointer
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+/** finding the size of the string**/
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		if (counter % 2 == 0)
+		{
+			_putchar(str[counter]);
+		}
+		counter++;
+	}
+	_putchar('\n');
+}
