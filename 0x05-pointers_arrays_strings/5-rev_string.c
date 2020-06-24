9f17e29ce6f12ee132f894df13845cb4de278fc2
Andres802 <reachandres@gmail.com>
2020-06-24 01:05:07 +0000

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,3 +5,21 @@
  * @s: char array string type
  */
 void rev_string(char *s)
+{
+	int i, j; temp;
+
+	i = 0;
+	while (s[i] =! '\0')
+	{
+		i ++;
+	}
+	i--; /** get the last position of the array before null */
+	j = 0;
+	while (j < i)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i--;
+		j++:
+	}
