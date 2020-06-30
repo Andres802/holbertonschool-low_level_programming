883c8f3f1a334fe1d8cb802ac461b5c21ba3425e
Andres802 <reachandres@gmail.com>
2020-06-30 03:18:23 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: array
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar ('\n');
+	}
+}
