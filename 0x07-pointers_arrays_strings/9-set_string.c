0a887efd435c3bf6655cdb1fb06eb5c22c7c4b8f
Andres802 <reachandres@gmail.com>
2020-07-01 02:28:55 +0000

    Double pointer referencing

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _strstr - function that locates a substring
+ * @s: pointer to a char s0
+ * @to: pointer to a char s1
+ * Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
