3b391b2766546e1db03c52fd16d5f0ea2132767a
Andres802 <reachandres@gmail.com>
2020-07-08 04:55:28 +0000

    Malloc free a 2 dimensional grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: integer parameter
+ * @height: integer parameter
+ * Return: int pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int x, y;
+	int **ptr2 = NULL;
+	/** chech if parameter are 0 or less */
+	if (width <= 0 && height <= 0)
+	{
+		return (NULL);
+	}
+	/** create memory for my malloc type double pointer */
+	ptr2 = malloc(sizeof(int *) * height);
+	/** check if my ptr is on succes */
+	if (ptr2 == NULL)
+	{
+		return (NULL);
+	}
+	/** opening spaces in the height for the width in each height*/
+	for (x = 0 ; x < height ; x++)
+	{
+		/** opening spaces for the width*/
+		ptr2[x] = malloc(sizeof(int) * width);
+		if (ptr2[x] == NULL)
+		{
+			for (; x >= 0 ; x--)
+			{
+				free(ptr2[x]);
+			}
+			free(ptr2);
+			return (NULL);
+		}
+	}
+	/** fill the array both dimensions with 0 */
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			ptr2[x][y] = 0;
+	}
+	return (ptr2);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,5 +8,7 @@ int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
 int getsize1(char *s1);
 int getsize2(char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
