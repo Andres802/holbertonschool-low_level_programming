40b48c313a7982c424ad3713ce5bd12518c6c78c
Andres802 <reachandres@gmail.com>
2020-07-08 01:51:45 +0000

    Copy new string in to new allocation memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - Returns a pointer to a newly allocated space using malloc
+* @str: Firts input int argument "Holberton"
+* Description: Returns a pointer to a newly allocated space
+* in memory, ofthe string given as a parameter.
+* Return: null or a pointer
+*/
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int recorrido = 0;
+	unsigned int len;
+
+	if (str == '\0')
+	return (0);
+	len = _strlen(str);
+	p = malloc(len + 1 * (sizeof(char)));
+	if (p == '\0')
+	return (0);
+	for (; recorrido < len; recorrido++)
+	p[recorrido] = str[recorrido];
+	return (p);
+}
+/**
+* _strlen - Returns a pointer to a newly allocated space using malloc
+* @s: Input int argument "Holberton"
+* Return: null or a pointer
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	for (length = 0; length < *s; length++)
+	s++;
+	return (length);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,5 +2,8 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+int _strlen(char *s);
+
 #endif
