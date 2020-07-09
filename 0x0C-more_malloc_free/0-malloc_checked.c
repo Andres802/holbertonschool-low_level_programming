6421847e181de33f2afd2f4f2e091e40a8c14156
Andres802 <reachandres@gmail.com>
2020-07-09 23:24:17 +0000

    malloc_checked exit 98

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * *malloc_checked- Write a function
+ * that allocates memory using malloc function
+ * @b: unsigned int parameter
+ * Return: newptr
+ * /
+ */
+void *malloc_checked(unsigned int b)
+{
+	/** declare my pointer to reserve int to the size of my argument*/
+	void *newptr = 0;
+
+	newptr = malloc(b);
+	/** Next I do check if new pointer fails*/
+	if (newptr == 0)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (newptr);
+	}
+}
