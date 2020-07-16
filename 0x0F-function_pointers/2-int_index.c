d060ff80139b587e1a4f3c2460c4854219c1b4ed
Andres802 <reachandres@gmail.com>
2020-07-16 01:16:53 +0000

    Return the value of the position of the array pointer function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - check conditios is true with the number of the position of array
+ * so basically seacrh fot the integer 
+ * @array: This is the pointer of the array
+ * @acmp: the pointer to the function
+ * @size: Size of the array
+ * Return: Nothing because is a void function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int c; /*counter*/
+
+	if (array == '\0' || size <= '\0' || cmp == '\0')
+	return (-1);
+
+	for (c = 0; c < size; c++)
+	if (cmp(array[c]))
+	return (c);
+	return (-1);
+}
