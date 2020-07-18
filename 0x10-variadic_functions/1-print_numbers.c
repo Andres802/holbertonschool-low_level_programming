adeb2ba1f987a8b61196ed8961fc47e53dc49b64
Andres802 <reachandres@gmail.com>
2020-07-18 18:50:28 +0000

    New code fix i < n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(valist, n);
-	/*if (n <= 0)
-		return;*/
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
