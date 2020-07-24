913a1d480084961c0bb4511c56e1f202cad2f092
Andres802 <reachandres@gmail.com>
2020-07-24 05:15:30 +0000

    Betty fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,16 +2,16 @@
 /**
  * free_list - will free the memory used by the structure
  *@head: heaad of my list struct
- * Return: Void 
- */ 
+ * Return: Void
+ */
 void free_list(list_t *head)
 {
 list_t *temp;
 
 	while (head != NULL)
 	{
-    	temp = head ->next;
-    	free(head->str);
+		temp = head->next;
+		free(head->str);
 		free(head);
 		head = temp;
 	}
