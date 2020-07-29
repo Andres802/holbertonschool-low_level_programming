24c8697c41658c54d7e3f41d5de71a869eb71a04
Andres802 <reachandres@gmail.com>
2020-07-29 02:19:05 +0000

    betty stule fix

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - this fuction will
- * delete a node at any given position
+ * reverse_listint - resverse the link list
  * @head: the beggining of the linked list head
  * Return: return a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *next =NULL; listint_t *before = NULL;
+	listint_t *next = NULL;
+	listint_t *before = NULL;
 
 if (head == NULL)
 {
 	return (NULL);
 }
 
-while (*head)
+while (*head != NULL)
 {
 	next = (*head)->next;
 	(*head)->next = before;
