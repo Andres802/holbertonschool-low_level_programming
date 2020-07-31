814aa0fda4f9dcf001cf4807d92a14789d33fe29
Andres802 <reachandres@gmail.com>
2020-07-31 03:51:35 +0000

    fix header and comments betty style

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * clear_bit - Function that changes the bit for 0 in the respctive index
- * @void: variable
+ * get_endianness- Function that check endianess
+ * @void: void
  * Return: Always 0if big endian or 1 if little indian
  */
 int get_endianness(void)
-{ 
-   unsigned int i = 1; 
-   char *c = (char*)&i; 
-   return (*c ? 1 : 0);
+{
+unsigned int i = 1;
+char *c = (char *)&i;
+return (*c ? 1 : 0);
 }
