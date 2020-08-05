aa6a1ce2eecd0d4be8a396f810086ea48fddda2e
Andres802 <reachandres@gmail.com>
2020-08-05 03:55:26 +0000

    FILE FIX

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,14 +16,22 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_src file_dest\n"),
 	exit(97);
-	if (argv[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-		argv[1]), exit(98);
+	/*if (argv[1] == NULL)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+	argv[1]), exit(98);
 	if (argv[2] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
-		argv[2]), exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+	argv[2]), exit(99);*/
+
 	file_src = open(argv[1], O_RDONLY);
 	file_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (file_src == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+		argv[1]), exit(98);
+	if (file_dest == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
+		argv[2]), exit(99);
 	rd = read(file_src, buffer, 1024);
 	if (rd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
