9c5caedec7067a1f9b6b6252d81354527cce80af
Andres802 <reachandres@gmail.com>
2020-08-05 00:58:40 +0000

    betty fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,29 +14,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(buffer) * letters);
 
 	if (buffer == NULL || filename == NULL)
-
-			return (0);
+		return (0);
 
 	i = open(filename, O_RDONLY);
-		if (i == -1)
-		{
-			free(buffer);
-			return (0);
-		}
+	if (i == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 	j = read(i, buffer, letters);
-		if (j == -1)
-		{
-			free(buffer);
-			close(i);
-			return (0);
-		}
+	if (j == -1)
+	{
+		free(buffer);
+		close(i);
+		return (0);
+	}
 	k = write(STDOUT_FILENO, buffer, j);
-		if (k == -1)
-		{
-			free(buffer);
-			close(i);
-			return (0);
-		}
+	if (k == -1)
+	{
+		free(buffer);
+		close(i);
+		return (0);
+	}
 	free(buffer);
 	close(i);
 	return (k);
