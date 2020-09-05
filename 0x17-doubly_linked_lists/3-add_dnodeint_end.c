0882b8b5131ff35365c447a754803dd2117a354b
Andres802 <reachandres@gmail.com>
2020-09-05 19:34:36 +0000

    Fix added in edge cases

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,11 +17,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 	while (temp->next != NULL)
 		temp = temp->next;
 		temp->next = new_node;
+		new_node->prev = temp;
 		return (new_node);
 }
