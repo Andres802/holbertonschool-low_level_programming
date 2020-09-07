fbb2f14251a7194d0ed151c77119d0e78ae96431
Andres802 <reachandres@gmail.com>
2020-09-07 02:11:46 +0000

    delete node code linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - this fuction will
+ * delete a node at any given position
+ * @head: the beggining of the linked list head
+ * @index: index position of te node to delete
+ * Return: 1 if it succeed or -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *nodedelete;
+	unsigned int j;
+
+	j = 0;
+
+	while (*head)
+	{
+		if (j == index)
+		{
+			nodedelete = (*head);
+			*head = (*head)->next;
+			free(nodedelete);
+			return (1);
+		}
+		head = &(*head)->next;
+		j++;
+	}
+	return (-1);
+}
