d7b7e331885e8acaca6c9a3573d8c6522440bb0c
Andres802 <reachandres@gmail.com>
2020-09-08 05:14:42 +0000

    Task adv

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/gedafu/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/raffle.so
+wget -P /tmp/ https://raw.githubusercontent.com/Andres802/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/raffle.so
 export LD_PRELOAD=/tmp/raffle.so
