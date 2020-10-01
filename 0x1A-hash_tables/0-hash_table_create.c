70863ee3eaf2a186a8618142be993f8a1f48bcb8
Andres802 <reachandres@gmail.com>
2020-10-01 21:07:55 +0000

    comments added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h_t = NULL;
 	hash_node_t **h_a = NULL;
 
+	/**create and allocate memory space of the node**/
 	h_a = calloc(size, sizeof(char *));
 	if (h_a == NULL)
 	{
 		free(h_a);
 		return (NULL);
 	}
-
+	/**create and allocate memory for the future hash table**/
 	h_t = calloc(1, sizeof(hash_table_t));
 	if (h_t == NULL)
 	{
@@ -26,6 +27,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
+	/** set the size and array in the new hast table**/
 	h_t->size = size;
 	h_t->array = h_a;
 	return (h_t);
