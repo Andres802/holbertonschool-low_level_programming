6d2c451f59e023a6f7c5e613d8a8d4975ee987d6
Andres802 <reachandres@gmail.com>
2021-01-14 17:19:14 +0000

    interpolation seacrh algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int aux_binary_search(int *array, int low, int high, int value);
 
 #endif /* SEARCH_ALGOS_H */
